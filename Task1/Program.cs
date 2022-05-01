51259166a8e4cf87a5f72842c74f11617ad1634b
edochik <edoch@yandex.ru>
2022-05-01 18:46:57 +0300

    add information task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,14 +1,68 @@
 ﻿// Найти сумму чисел от 1 до А
+/*
+int n = 100;
+
 
 int Method(int a)
 {
-    int n = 0;
-    n = n*(n+1);
+    n = n * (n + 1) / 2;
     return n;
 }
 
-int b = 10; 
-int c = b*(b+1);
 
-Console.WriteLine(Method(b));
-Console.WriteLine(c);
+int Method2(int b)
+{
+    int result = 0;
+    for (int i = 0; i < 1; i++)
+    {
+        result = result + b;
+        //Console.Write($" {result}");
+    }
+    return result;
+}
+
+Console.WriteLine(Method(n));
+Console.WriteLine(Method2(n));*/
+using System.Diagnostics;
+
+int a = 100_000;
+
+int var1(int n)
+{
+    int[] arr = new int[n + 1];
+    for (int i = 0; i <= n; i++)
+    {
+        arr[i] = i;
+    }
+    int res = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        res += arr[i];
+    }
+    return res;
+}
+
+int var2(int n)
+{
+    int res = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        res += i;
+    }
+    return res;
+}
+
+int var3(int n)
+{
+    n = n * (n + 1) / 2;
+    return n;
+}
+
+Stopwatch sw = new Stopwatch();
+sw.Start();
+Console.WriteLine(var1(a));
+sw.Stop();
+Console.WriteLine(sw.ElapsedMilliseconds);
+Console.WriteLine(var2(a));
+
+Console.WriteLine(var3(a));
